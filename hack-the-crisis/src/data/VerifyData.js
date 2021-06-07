c58911bedbb07b8c8fc9d857c178caf9cf8068b7
AAVAIG2001 <aavaigmalhotra2001@gmail.com>
2021-06-07 16:43:00 +0530

    Add files via upload

---
+++ hack-the-crisis/src/data/VerifyData.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export const data = [
+  {
+    number: '1',
+    title: 'Check the seal',
+    info: 'The security seal, especially in bottled medicines, should not be damaged or tampered. Look for a crack or break in the sealing tape before making the purchase. If you feel it is broken or tampered, do not buy it.',
+  },
+
+  {
+    number: '2',
+    title: 'Check the packaging',
+    info: 'This is perhaps the simplest method. If it is a medicine you have purchased previously, look at the font, spellings, and the colour. Most often, one or all of these are wrong in a fake medicine.',
+  },
+  {
+    number: '3',
+    title: 'Visual inspection of medicines',
+    info: `As stated by the World Health Organisation, the common physical attributes that need to be looked up in medicine tablets are:
+
+     Small pieces of tablets or excessive powder at the bottom of the medicine container,
+     Cracks in the tablets,
+     The appearance of crystals on the container walls or the medicine tablets,
+     Softening or hardening of the medicines,
+     Swelling, spots or discolouration of the tablets
+    `,
+  },
+  {
+    number: '4',
+    title: ' Price',
+    info: 'If there is a sudden dip in the price of the medicines, then be sure to double-check. The chances in such a case can mean that it is a spurious drug. Medicines are usually manufactured in comparatively the same manner; hence, changes in the pricing of one brand can greatly affect all other pharmaceutical companies. So beware!',
+  },
+  {
+    number: '5',
+    title: 'Unexpected side-effects or allergies',
+    info: 'The immediate impact that counterfeit drugs can have on us is an allergic reaction or a sudden unexpected side-effect. The spurious drugs may contain incorrect substances, improper dosage or hazardous substances, which do not elicit any therapeutic effect. If you experience any unforeseen side-effects, stop consuming the medicine immediately and check with your doctor.',
+  },
+  {
+    number: '6',
+    title: ' Source',
+    info: 'The source of the drug also determines if you are buying a fake drug or not. Filling your prescription in a reputable pharmacy greatly reduces your chances of buying fake drugs while buying from illiterate and unqualified vendors who hawk drugs in buses, motor parks and in the streets increases your chances of buying fake drugs.',
+  },
+];
